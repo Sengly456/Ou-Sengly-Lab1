7032df3e408a41f0eb0ff84f3a0acdc6e8a08389
Sengly456 <senglyou84@gmail.com>
2025-11-04 10:31:52 +0700

    Initial Node.js project with test and lint

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 12,
+      sourceType: "module",
+      globals: {
+        ...globals.node,
+        ...globals.mocha,
+      },
+    },
+    rules: {
+   
+    },
+  },
+];
